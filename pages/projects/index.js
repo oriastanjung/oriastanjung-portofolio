1f4a5556d0ed267f4cc364e9dcb54b0a05987d15
O. Riastanjung <oriastan999@gmail.com>
2022-08-10 19:52:11 +0700

    Adding JSON Dummy Data Projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,15 +2,25 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import ApplicationWindow from '../../components/ApplicationWindow'
 import projectIcon from "../../assets/projects.png";
+import dataProject from "../../json/dataProjects.json";
 
 function projects() {
+  
+
   return (
     <Layout>
       <main>
         <ApplicationWindow iconWindow={projectIcon} windowTitle={'Projects'}>
-          <h1 style={{color:"white"}}>
-            hellooooo
-          </h1>
+          <ul>
+
+          {
+            dataProject.map((item) => {
+              return (<li key={item.id} style={{color:'white'}}>
+                {item.title}
+              </li>)
+            })
+          }
+          </ul>
         </ApplicationWindow>
       </main>
     </Layout>
