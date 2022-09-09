98795c4105ff83cdc154dc2a0afb22be9efdb02b
O. Riastanjung <oriastan999@gmail.com>
2022-09-09 19:40:35 +0700

    add certificates

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,16 +1,44 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Taskbar from "../Taskbar";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import Head from "../Head";
-
 function Layout(props) {
   const router = useRouter();
+  const date= new Date();
+  const hour = Number(date.getHours());
+  const wallpaperBG = [
+    "/bg-01.gif",
+    "/bg-02.gif",
+    "/bg-03.jpeg",
+    "/bg-04.jpeg",
+    '/bg-05.jpg',
+    '/bg-06.png'
+  ];
 
-
-
+  let background;
+  const chooseRandomBG = () => {
+    if(hour >= 8 && hour <10){
+      return background = wallpaperBG[3]
+    }else if(hour >= 10 && hour < 14){
+      return background = wallpaperBG[5]
+    }else if(hour >= 14 && hour < 17){
+      return background = wallpaperBG[4]
+    }else if(hour >= 17 && hour < 19){
+      return background = wallpaperBG[2]
+    }else if(hour >= 19 && hour <= 24){
+      return background = wallpaperBG[1]
+    }else{
+      return background = wallpaperBG[0]
+    }
+  }
+  chooseRandomBG();
   return (
-    <div className="background-img">
+
+    <div
+      className="background-img"
+      style={{ backgroundImage: `url("${background}")` }}
+    >
       <Head></Head>
       <Navbar></Navbar>
       {props.children}
--- pages/certificates/index.js
+++ pages/certificates/index.js
@@ -1,21 +1,144 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import ApplicationWindow from '../../components/ApplicationWindow'
+import React from "react";
+import Layout from "../../components/Layout";
+import ApplicationWindow from "../../components/ApplicationWindow";
 import certificatesIcon from "../../assets/certificates.png";
+import Certificate1IMG from "../../assets/certificates/certificate-01.png";
+import Certificate2IMG from "../../assets/certificates/certificate-02.png";
+import Certificate3IMG from "../../assets/certificates/certificate-03.png";
+import Certificate4IMG from "../../assets/certificates/certificate-04.png";
+import Certificate5IMG from "../../assets/certificates/certificate-05.png";
+import Certificate6IMG from "../../assets/certificates/certificate-06.png";
+import Certificate7IMG from "../../assets/certificates/certificate-07.png";
+import Image from "next/image";
 
+
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 
 function certificates() {
   return (
     <Layout>
       <main>
-      <ApplicationWindow iconWindow={certificatesIcon} windowTitle={'Certificates'}>
-          <h1 style={{color:"white"}}>
-            Certificates
-          </h1>
+        <ApplicationWindow
+          iconWindow={certificatesIcon}
+          windowTitle={"Certificates"}
+        >
+          <div className="swiper-container">
+            <Swiper
+              // install Swiper modules
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={0}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log("slide change")}
+            >
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate1IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate2IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate3IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate4IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate5IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate6IMG} />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="certificates-img"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Image src={Certificate7IMG} />
+                </div>
+              </SwiperSlide>
+              
+            </Swiper>
+          </div>
         </ApplicationWindow>
       </main>
     </Layout>
-  )
+  );
 }
 
-export default certificates
+export default certificates;
